3d17af24dc4c5cb788c8ea32bfe60bf5929722fe
Diego Gomes <dsgomes1990@gmail.com>
2021-08-03 22:48:21 -0300

    Api project created

---
+++ oferta-api/oferta-api-solution/src/oferta-api/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace oferta_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ILogger<ProdutosController> _logger;
+
+        public ProdutosController(ILogger<ProdutosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
---
+++ oferta-api/oferta-api-solution/src/oferta-api/Controllers/VendasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace oferta_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VendasController : ControllerBase
+    {
+        private readonly ILogger<VendasController> _logger;
+
+        public VendasController(ILogger<VendasController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+    }
+}
